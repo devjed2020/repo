914e335128c24edcc313230103f806da5daec8cd
ArteezyOne <61806280+ArteezyOne@users.noreply.github.com>
2020-06-17 15:30:18 -0500

    Update login.php

--- vistas/login.php
+++ vistas/login.php
@@ -9,6 +9,18 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    
+    <form action="" method="POST">
+        <?php
+            if(isset($errorLogin)){
+                echo $errorLogin;
+            }
+        ?>
+        <h2>Iniciar sesión</h2>
+        <p>Nombre de usuario: <br>
+        <input type="text" name="username"></p>
+        <p>Password: <br>
+        <input type="password" name="password"></p>
+        <p class="center"><input type="submit" value="Iniciar Sesión"></p>
+    </form>
 </body>
 </html>
